0f8d6cb32c859b2eeb64b6b150dcae1b74d61f8f
Ihor Salnikov <ihor.salnikov@gmail.com>
2024-10-14 22:44:10 +0200

    up x2

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
-import React, {createRef, Suspense, useRef, useState} from 'react'
+import React from 'react'
 import './App.css'
-import {Canvas, useFrame} from "@react-three/fiber";
-import {GizmoHelper, GizmoViewport, OrbitControls, useGLTF} from "@react-three/drei";
-import {Debug, Physics, useBox, useCylinder, usePlane} from '@react-three/cannon'
 
 function App() {
     return <>
