9f6e8c034db977e0221a92e25be693baa9123435
Ihor Salnikov <ihor.salnikov@gmail.com>
2024-10-14 22:34:08 +0200

    up

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,165 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import React, {createRef, Suspense, useRef, useState} from 'react'
 import './App.css'
+import {Canvas, useFrame} from "@react-three/fiber";
+import {GizmoHelper, GizmoViewport, OrbitControls, useGLTF} from "@react-three/drei";
+import {Debug, Physics, useBox, useCylinder, usePlane} from '@react-three/cannon'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+function Coin(props) {
+  const {nodes: nodes} = useGLTF('/coin.glb');
+  let [ref] = useCylinder(() => ({
+    mass: 1,
+    args: [1, 1, .25, 30],
+    position: props.position,
+    type: "Dynamic",
+    sleepSpeedLimit: .1,
+    sleepTimeLimit: 1,
+    allowSleep: true,
+    angularDamping: 0.2,
+    linearDamping: 0.2,
+  }));
+
+  return <>
+    <mesh ref={ref}
+          geometry={nodes.COIN.geometry}
+          {...props}>
+      <meshStandardMaterial
+        color="gold"
+        roughness={.5}
+        metalness={1}
+        envMapIntensity={1}
+      />
+    </mesh>
+  </>
+}
+
+
+function Machine(props) {
+  const [boxRef1, api] = useBox(() => ({
+    mass: 1,
+    args: [1, 1, 1],
+    rotation: [0, 0, Math.PI / 4],
+    position: [1, 1, 1]
+  }));
+
+  useFrame((state, delta, frame) => {
+    api.rotation.set(frame * 2, frame * 2, frame * 2)
+
+  })
+  return <>
+    <mesh
+      ref={boxRef1}
+    >
+
+      <meshStandardMaterial
+        color="gold"
+        roughness={.5}
+        metalness={1}
+        envMapIntensity={1}
+      />
+    </mesh>
+
+  </>
+}
+
+function Plane(props) {
+  const [ref] = usePlane(() => ({rotation: [-Math.PI / 2, 0, 0], type: "Static", ...props}))
+  useFrame(() => {
+
+  })
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <mesh ref={ref} color='red'>
+      <planeGeometry args={[100, 100]}/>
+      <meshStandardMaterial color="red" roughness={1}/>
+    </mesh>
   )
 }
+
+function PusherBlock(props) {
+  const [ref] = useBox(() => ({rotation: [-Math.PI / 2, 0, 0], ...props}))
+  return (
+    <mesh ref={ref} color='red'>
+      <planeGeometry args={[100, 100]}/>
+      <meshStandardMaterial color="red" roughness={1}/>
+    </mesh>
+  )
+}
+
+function App() {
+  let ref = useRef(null);
+  const [dpr, setDpr] = useState(0)
+
+  let useState1 = useState(1);
+
+  const meshRefs = React.useRef([...Array(useState1[0])].map(() => createRef()))
+
+  return (
+    <>
+      {/*<div>*/}
+      {/*  <button onClick={() => addCoin()}></button>*/}
+      {/*</div>*/}
+      <Canvas
+        ref={ref}
+        camera={{position: [2, 5, 5], fov: 30}}
+        style={{
+          backgroundColor: '#2b7abb',
+          width: '100vw',
+          height: '100vh',
+        }}
+      >
+        <color attach="background" args={['lightpink']}/>
+        <spotLight
+          position={[15, 40, 15]}
+          angle={0.3}
+          penumbra={1}
+          intensity={2}
+          castShadow
+          shadow-mapSize-width={2048}
+          shadow-mapSize-height={2048}
+        />
+        <pointLight intensity={1} position={[0, 5, 0]}/>
+        <OrbitControls autoRotate={false}/>
+        <Suspense fallback={null}>
+          <Physics
+            allowSleep={true}
+            iterations={100} // Increase the number of solver iterations
+            tolerance={0.001} // Set a lower tolerance for solver error
+          >
+            <Debug color='red' scale={1}>
+              <Plane></Plane>
+              <group>
+                <Machine></Machine>
+                {/*{meshRefs.current.map((coin, index) => <Coin ref={coin} key={index} position={[0, 6, 0]}></Coin>)}*/}
+                {/*{meshRefs.current.map((coin, index) => <Coin ref={coin} key={index} position={[0, 6, 0]}></Coin>)}*/}
+                {/*{meshRefs.current.map((coin, index) => <Coin ref={coin} key={index} position={[0, 6, 0]}></Coin>)}*/}
+                {/*{meshRefs.current.map((coin, index) => <Coin ref={coin} key={index} position={[0, 6, 0]}></Coin>)}*/}
+                {/*{meshRefs.current.map((coin, index) => <Coin ref={coin} key={index} position={[0, 6, 0]}></Coin>)}*/}
+                {/*{meshRefs.current.map((coin, index) => <Coin ref={coin} key={index} position={[0, 6, 0]}></Coin>)}*/}
+                {/*{meshRefs.current.map((coin, index) => <Coin ref={coin} key={index} position={[0, 6, 0]}></Coin>)}*/}
+                {/*{meshRefs.current.map((coin, index) => <Coin ref={coin} key={index} position={[0, 6, 0]}></Coin>)}*/}
+              </group>
+            </Debug>
+          </Physics>
+        </Suspense>
+
+        <GizmoHelper alignment="bottom-right" margin={[80, 80]}>
+          <GizmoViewport axisColors={['#9d4b4b', '#2f7f4f', '#3b5b9d']} labelColor="white"/>
+        </GizmoHelper>
+
+      </Canvas>
+    </>
+  );
+}
+
+const useRefs = () => {
+  const refs = useRef<Record<string, HTMLElement | null>>({})
+
+  const setRefFromKey = (key: string) => (element: HTMLElement | null) => {
+    refs.current[key] = element;
+  }
+
+  return {refs: refs.current, setRefFromKey};
+}
+
 
 export default App
---
+++ src/Model.tsx
@@ -0,0 +1,14 @@
+import React, { useRef, useState } from "react";
+import { useLoader, useFrame } from "@react-three/fiber";
+
+const GltfModel = ({ modelPath, scale = 40, position = [0, 0, 0] }) => {
+
+  // Subscribe this component to the render-loop, rotate the mesh every frame
+  return (
+    <>
+
+    </>
+  );
+};
+
+export default GltfModel;
